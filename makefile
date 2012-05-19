597491ba9f26cc670967e1e68ed9ca0427b95373
Trevor R <tarice.dev@gmail.com>
2012-05-19 04:39:06 -0500

    Simplified the INSTALL/README; removed excess data that can be regenerated using the modified update_html_data_file script or the program itself; updated makefile in anticipation for adding JSON export file format.

--- makefile
+++ makefile
@@ -1,17 +1,22 @@
 SRC_DIR = source
-SRC = $(SRC_DIR)/stb_image.c $(SRC_DIR)/webtap.c
-OBJ = $(SRC_DIR)/stb_image.o $(SRC_DIR)/webtap.o
+SRC = $(SRC_DIR)/stb_image.c $(SRC_DIR)/webtap.c $(SRC_DIR)/webtap_timer.c
+OBJ = $(SRC_DIR)/stb_image.o $(SRC_DIR)/webtap.o $(SRC_DIR)/webtap_timer.o
+LIB = -lm -lmxml -ljson -lcurl -Wl,-Bsymbolic-functions -lpthread
+FLAGS = -Wunused -DXML_OUTPUT -DJSON_OUTPUT
 
-all: image.x
+all: webtap
 
-image.x : $(OBJ)
-	gcc -lm $(OBJ) -o $@ -lmxml -lpthread
+webtap: $(OBJ)
+	clang -Wl, $(OBJ) -o $@ $(LIB) $(FLAGS)
+
+debug: $(OBJ)
+	clang $(OBJ) -o $@ $(LIB) -DDEBUG=1 $(FLAGS)
+
+test: $(OBJ)
+	clang $(OBJ) -o $@ $(LIB) -DLIBDIR=1 -Wl,libdir $(FLAGS)
 
 %.o : %.c
-	gcc -c $< -o $@
-
-test:
-	gcc -lm $(OBJ) -o $@ -lmxml -lpthread -LLIBDIR -Wl,libdir
+	clang -c $< -o $@ -DDEBUG $(FLAGS)
 
 clean:
 	rm -f $(SRC_DIR)/*.o
--- scripts/update_html_data_file.sh
+++ scripts/update_html_data_file.sh
@@ -2,11 +2,14 @@
 # This script updates the data stored for each city, pulled from the html.
 # This should be run once, to generate the file, and after that only if cities are added to the map, or their location is moved.
 # Otherwise, this script will just be using bandwidth to generate the same data.
-html_data="../data/html_data/"
+all_data="../data"
+html_data=$all_data"/html_data"
+# Download the .html files.
+curl 'http://map.wwiionline.com/{holland,channel,belgium,central_german,ne_france,maginot}_supply.html' -o "$html_data/#1_supply.html" 
 # The order is unimportant; make sure that the integers assigned correspond to the image_data indices, though.
-grep -r 'option value' $html_data/holland_supply.html | sed -e 's/^.*<option value=//g' -e 's@</option>@@g' -e 's@"@1;(@1' | sed -e 's@"@)@1' -e 's/>/;/g' >$html_data/city_data
-grep -r 'option value' $html_data/channel_supply.html | sed -e 's/^.*<option value=//g' -e 's@</option>@@g' -e 's@"@2;(@1' | sed -e 's@"@)@1' -e 's/>/;/g' >>$html_data/city_data
-grep 'option value' $html_data/belgium_supply.html | sed -e 's/^.*<option value=//g' -e 's@</option>@@g' -e 's@"@3;(@1' | sed -e 's@"@)@1' -e 's/>/;/g' >>$html_data/city_data
-grep -r 'option value' $html_data/central_german_supply.html | sed -e 's/^.*<option value=//g' -e 's@</option>@@g' -e 's@"@4;(@1' | sed -e 's@"@)@1' -e 's/>/;/g' >>$html_data/city_data
-grep -r 'option value' $html_data/ne_france_supply.html | sed -e 's/^.*<option value=//g' -e 's@</option>@@g' -e 's@"@5;(@1' | sed -e 's@"@)@1' -e 's/>/;/g' >>$html_data/city_data
-grep -r 'option value' $html_data/maginot_supply.html | sed -e 's/^.*<option value=//g' -e 's@</option>@@g' -e 's@"@6;(@1' | sed -e 's@"@)@1' -e 's/>/;/g' >>$html_data/city_data
+grep -r 'option value' $html_data/holland_supply.html | sed -e 's/^.*<option value=//g' -e 's@</option>@@g' -e 's@"@1;(@1' | sed -e 's@"@)@1' -e 's/>/;/g' >$all_data/city_data
+grep -r 'option value' $html_data/channel_supply.html | sed -e 's/^.*<option value=//g' -e 's@</option>@@g' -e 's@"@2;(@1' | sed -e 's@"@)@1' -e 's/>/;/g' >>$all_data/city_data
+grep 'option value' $html_data/belgium_supply.html | sed -e 's/^.*<option value=//g' -e 's@</option>@@g' -e 's@"@3;(@1' | sed -e 's@"@)@1' -e 's/>/;/g' >>$all_data/city_data
+grep -r 'option value' $html_data/central_german_supply.html | sed -e 's/^.*<option value=//g' -e 's@</option>@@g' -e 's@"@4;(@1' | sed -e 's@"@)@1' -e 's/>/;/g' >>$all_data/city_data
+grep -r 'option value' $html_data/ne_france_supply.html | sed -e 's/^.*<option value=//g' -e 's@</option>@@g' -e 's@"@5;(@1' | sed -e 's@"@)@1' -e 's/>/;/g' >>$all_data/city_data
+grep -r 'option value' $html_data/maginot_supply.html | sed -e 's/^.*<option value=//g' -e 's@</option>@@g' -e 's@"@6;(@1' | sed -e 's@"@)@1' -e 's/>/;/g' >>$all_data/city_data
